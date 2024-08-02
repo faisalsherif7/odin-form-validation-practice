0946125b8a0ebfc8e1ead4f9d1d8a41d37a3f151
Faisal Sherif <116718026+faisalsherif7@users.noreply.github.com>
2024-08-02 21:33:06 +0530

    Separate validation logic into a diff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 import { intializeDropdowns } from './dropdown';
-
-let country = null
+import { validateEmail, validatePassword, validateZipcode } from './validate';
 
 intializeDropdowns()
 validateEmail()
@@ -10,25 +9,19 @@ validateZipcode()
 validatePassword()
 submitForm()
 
-function validateEmail() {
-    email.addEventListener("input", (event) => {
-        const emailSpan = document.querySelector('.email-span')
-        emailSpan.classList.remove('default-message')
-        email.classList.remove('invalid')
-        if (email.value === "") {
-            emailSpan.classList.remove('error')
-            emailSpan.classList.add('default-message')
-            emailSpan.textContent = "Format a@b" 
-            return
-        }
-        if (email.validity.valid) {
-            emailSpan.classList.remove('error')
-            emailSpan.textContent = "✅"
-        } else {
-            emailSpan.classList.add('error')
-            emailSpan.textContent = "Please provide a valid email address"
-        }
-    })
+let country = null
+
+function selectCountry() {
+    document.querySelectorAll('.dropdown-item').forEach((dropdownItem) => {
+        dropdownItem.addEventListener('click', (event) => {
+            country = event.target.textContent
+            document.querySelector('.dropdown-button').classList.remove('invalid-country')
+            const span = document.querySelector('.country-span')
+            span.classList.remove('error')
+            span.classList.add('default-message')
+            span.textContent = "✅"
+        })
+    }) 
 }
 
 function validateCountry() {
@@ -80,59 +73,4 @@ function submitForm() {
         }
     })
 
-}
-
-function selectCountry() {
-    document.querySelectorAll('.dropdown-item').forEach((dropdownItem) => {
-        dropdownItem.addEventListener('click', (event) => {
-            country = event.target.textContent
-            document.querySelector('.dropdown-button').classList.remove('invalid-country')
-            const span = document.querySelector('.country-span')
-            span.classList.remove('error')
-            span.classList.add('default-message')
-            span.textContent = "✅"
-        })
-    }) 
-}
-
-function validateZipcode() {
-    document.querySelector('#zipcode').addEventListener('input', () => {
-        const zipcodeSpan = document.querySelector('.zipcode-span')
-        zipcode.classList.remove('invalid')
-        zipcodeSpan.classList.remove('error')
-        if (zipcode.value === "") {
-            zipcodeSpan.classList.add('default-message')
-            zipcodeSpan.textContent = "Please provide a valid zipcode in the country you have selected"
-            return
-        }
-        if (!zipcode.validity.valid) {
-            zipcodeSpan.classList.add('error')
-            zipcodeSpan.classList.remove('default-message')
-            zipcodeSpan.textContent = "Please Provide a Valid Zipcode"
-        } else {
-            zipcodeSpan.classList.add('default-message')
-            zipcodeSpan.textContent = "✅"
-        }
-    })
-}
-
-function validatePassword() {
-    password.addEventListener('input', () => {
-        const passwordSpan = document.querySelector('.password-span')
-        password.classList.remove('invalid')
-        passwordSpan.classList.remove('error')
-        if (password.value === "") {
-            passwordSpan.classList.add('default-message')
-            passwordSpan.textContent = "Password must contain at least 10 characters, at least 1 number and 1 letter, and at least one special character"
-            return
-        }
-        if (password.validity.valid) {
-            passwordSpan.classList.add('default-message')
-            passwordSpan.textContent = "✅"
-        } else {
-            passwordSpan.classList.add('error')
-            passwordSpan.classList.remove('default-message')
-            passwordSpan.textContent = "Password must contain at least 10 characters, at least 1 number and 1 letter, and at least one special character"
-        }
-    })
 }
